c4e52e8ee130b0b3adbd20d18a2818c78f276a4b
BuildTools <unconfigured@null.spigotmc.org>
2022-02-02 14:52:54 -0500

    Commented out component-scan so the code can run. Still don't know what to put as base-package.

--- src/main/java/RuleEngine.java
+++ src/main/java/RuleEngine.java
@@ -2,7 +2,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
 
-@Component
 public class RuleEngine {
     public void executeRule(Function<String, Boolean> rule, String value){
         System.out.println(rule.apply(value));
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,8 +5,15 @@ public class main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext();
 
-        RuleEngine engine = (RuleEngine)context.getBean("ruleEngine");
+        RuleEngine engine = new RuleEngine();
 
-        System.out.println(main.class.getPackage().getName());
+//        RuleEngine engine = (RuleEngine)context.getBean("ruleEngine");
+
+        CheckBlank func = new CheckBlank();
+
+        String empty = "";
+        String notEmpty = "not an empty string";
+
+        engine.executeRule(func, empty);
     }
 }
